6259c517d6e0c8a09845b952b600d22700bcf8b8
David <55126143+devdavede@users.noreply.github.com>
2025-10-24 01:02:42 +0200

    Update sshd_config to disable ChallengeResponseAuthentication
    Append 'ChallengeResponseAuthentication no' to sshd_config as it might not exist in template

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -91,6 +91,7 @@ sudo sed -i 's/^#*PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/
 sudo sed -i 's/^#*PubkeyAuthentication.*/PubkeyAuthentication yes/' /etc/ssh/sshd_config
 sudo sed -i 's/^#*ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
 sudo sed -i 's/^#*PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+sudo echo \"ChallengeResponseAuthentication no\" | sudo tee -a /etc/ssh/sshd_config
 sudo systemctl restart sshd
 "
 
